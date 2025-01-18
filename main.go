e709737f8018d877a99ac5d63370487d48f51336
kimzey <kimkung0914@gmail.com>
2025-01-18 12:03:35 +0700

    pop os

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("is number  = ", fib(9))
+	fmt.Println("Hi Pop os", "Hello")
+}
 
-	fmt.Println("hello world")
-
+func fib(n int) int {
+	//Fibonacci
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
 }
